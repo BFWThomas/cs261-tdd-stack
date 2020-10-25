8b8ddf726dcfa0b367a045ca815528585ca7d07d
Brian Thomas <thomasb3@oregonstate.edu>
2020-10-25 00:30:37 -0700

    Fixed issue with peek and pop methods raising IndexError when not needed

--- stack.py
+++ stack.py
@@ -6,29 +6,32 @@ class Stack:
 
     def __init__(self):
         self.values = []
+        self.length = -1
 
     def is_empty(self):
         """
         Returns whether or not the stack is empty
         """
-        return len(self.values) == 0
+        return self.length == -1
 
     def pop(self):
         """
         Removes the last item on the stack
         """
-        if self.is_empty:
+        if self.is_empty():
             raise IndexError
 
     def peek(self):
         """
         Looks at the last item of the stack
         """
-        if self.is_empty:
+        if self.is_empty():
             raise IndexError
+        return self.values[self.length]
 
     def push(self, val):
         """
         Appends a new node to stack with input value
         """
         self.values += [val]
+        self.length += 1
