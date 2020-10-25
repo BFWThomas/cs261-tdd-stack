9fea9187dc628c61e45b880575f6f2faab31063b
Brian Thomas <thomasb3@oregonstate.edu>
2020-10-24 23:56:36 -0700

    Created is_empty method

--- stack.py
+++ stack.py
@@ -1,7 +1,14 @@
 # Stack: A stack.
 # Your implementation should pass the tests in stack.py.
-# YOUR NAME
+# Brian F Thomas
 
 class Stack:
 
-    pass
+    def __init__(self):
+        self.values = []
+
+    def is_empty(self):
+        """
+        Returns whether or not the stack is empty
+        """
+        return len(self.values) == 0
